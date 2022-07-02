f97107e48d7bed3c5e7fe4f17cb4db977fd9ce5e
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-07-03 00:12:44 +0530

    added tables

--- oj/models.py
+++ oj/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Problems(models.Model):
+    statement = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=50)
+
+class Solutions(models.Model):
+    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    verdict = models.CharField(max_length=10)
+    submitted_at = models.DateTimeField('attempt date')
+
+class testCases(models.Model):
+    input = models.CharField(max_length=500)
+    output = models.CharField(max_length=500)
+    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
