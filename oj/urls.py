dd0b8680cbc75c4aea5dd04988b545303dc54ef5
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-07-05 00:07:05 +0530

    designed detail screen

--- oj/urls.py
+++ oj/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='oj'
 urlpatterns = [
-    path('', views.problems, name = "problems")
+    path('', views.problems, name = "problems"),
+    path('problem/<int:problem_id>', views.problemDetail, name='problem_detail'),
 ]
