04fbcf8f129f00a4ebeb7dbf10ed80f516b7ce56
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-07-04 19:34:09 +0530

    added problem list view

--- oj/models.py
+++ oj/models.py
@@ -2,16 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Problems(models.Model):
-    statement = models.CharField(max_length=200)
+    statement = models.CharField(max_length=255)
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Solutions(models.Model):
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
     verdict = models.CharField(max_length=10)
     submitted_at = models.DateTimeField('attempt date')
+    submitted_code = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.verdict
 
 class testCases(models.Model):
     input = models.CharField(max_length=500)
     output = models.CharField(max_length=500)
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.input
--- oj/views.py
+++ oj/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Trial')
+from .models import Problems, Solutions
+
+def problems(request):
+    problems_list = Problems.objects.all()
+    context = {'problem_list':problems_list}
+    return render(request, 'oj/index.html', context)
+
+def problemDetail(request, problem_id):
+    problem = get_object_or_404(Problems, pk=problem_id)
+    context = {'problem': problem}
+    return render(request, 'oj/detail.html', context)
