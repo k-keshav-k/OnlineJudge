c4262bd30de1c89102e0e7b7d51f67879c5f61cd
Keshav Krishna <2019csb1224@iitrpr.ac.in>
2022-07-17 13:30:54 +0530

    changed debug & allowed all hosts

--- onlineJudge/settings.py
+++ onlineJudge/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--=ix)-%uyw@9-8+r)96!76vu+l6=)%3zf!fwvr*5)13xch0rn0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
